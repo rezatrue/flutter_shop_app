cbcc24c49b55cbc44bbc3407572237b84ec33d32
rezatrue <rezatrue@gmail.com>
2020-03-09 06:53:20 +0600

    216: Adding Manage products screen

---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class UseProductItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  UseProductItem({this.title, this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: CircleAvatar(backgroundImage: NetworkImage(imageUrl),),
+      trailing: Container(
+              width: 100,
+              child: Row(
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.edit), 
+            color: Theme.of(context).primaryColor, 
+            onPressed: () {},),
+            IconButton(icon: Icon(Icons.delete),
+            color: Theme.of(context).errorColor ,
+            onPressed: () {},),
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
