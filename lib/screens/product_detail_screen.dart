ccd0f4c583bd45e74a9f7b0ccab9cb5f746d207c
rezatrue <rezatrue@gmail.com>
2020-04-03 19:42:10 +0600

    282: Working with Slivers

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -14,29 +14,34 @@ static const routeName = '/product-detail';
     final productId = ModalRoute.of(context).settings.arguments as String;
     final loadedProduct = Provider.of<Products>(context, listen: false).findByid(productId);
     return Scaffold(
-      appBar: AppBar(title: Text(loadedProduct.title)),
-      body: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                    Container(
-                      height: 300,
-                      width: double.infinity,
-                      child: Hero(
+      //appBar: AppBar(title: Text(loadedProduct.title)),
+      body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 300,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(loadedProduct.title),
+                background: Hero(
                         tag: loadedProduct.id,
                         child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover)
                         ),
+                ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                  SizedBox(height: 10,),
+                  Text('\$${loadedProduct.price}', style: TextStyle(color: Colors.grey, fontSize: 20,), textAlign: TextAlign.center,),
+                  SizedBox(height: 10,),
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text('${loadedProduct.description}', textAlign: TextAlign.center, softWrap: true,)
                     ),
-                    SizedBox(height: 10,),
-                    Text('\$${loadedProduct.price}', style: TextStyle(color: Colors.grey, fontSize: 20),),
-                    SizedBox(height: 10,),
-                    Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Text('${loadedProduct.description}', textAlign: TextAlign.center, softWrap: true,)
-                      ),
-                ],
-                
-               ),
+                  SizedBox(height: 800,),
+              ])
+              ),
+          ],
       ),
     );
   }
